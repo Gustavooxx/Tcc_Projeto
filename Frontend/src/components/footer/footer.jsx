b7183b8bcb8915c90bb806c867eab5356dc480d7
j0ttaape <joaopedrotrajano59@gmail.com>
2025-10-11 18:35:33 -0300

    ajeitando footer

--- Frontend/src/components/footer/footer.jsx
+++ Frontend/src/components/footer/footer.jsx
@@ -1,33 +1,31 @@
 import './footer.scss'
 import { Link } from 'react-router'
 
-export default function Footer(){
-    return(
-         <footer className='footer'>
+export default function Footer() {
+    return (
+        <footer className='footer'>
 
             <div className='conteinar-foo'>
 
 
-            <div className='Nos'>
-                <h1>Doevida.com <img src="assets/images/image 4.svg" alt="" /></h1>
+                <div className='Nos'>
+                    <h1>Doevida.com <img src="assets/images/image 4.svg" alt="" /></h1>
 
-                <p> A Doevida.com é uma plataforma dedicada a facilitar o processo de doação de sangue, conectando doadores a hospitais e hemocentros.</p>
+                    <p> A Doevida.com é uma plataforma dedicada a facilitar o processo de doação de sangue, conectando doadores a hospitais e hemocentros.</p>
 
-            </div>
+                </div>
 
-            <div className='cadastro-foo'>
-                <p>Doe Vida</p>
+                <div className='cadastro-foo'>
+                    <p>Doe Vida</p>
 
-                <Link>
-                    <button>Cadastre-se</button>
-                </Link>
+                    <Link>
+                        <button>Cadastre-se</button>
+                    </Link>
 
-                <p>Entre em contato</p>
-                <p className='email'>contato@doevida.com</p>
+                    <p>Entre em contato</p>
+                    <p className='email'>contato@doevida.com</p>
 
-            </div>
-
-
+                </div>
 
             </div>
 
