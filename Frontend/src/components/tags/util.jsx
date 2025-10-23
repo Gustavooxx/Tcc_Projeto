28e2da646c1a6cd9e141a32b70f71f53308fb1c6
j0ttaape <joaopedrotrajano59@gmail.com>
2025-10-23 08:08:38 -0300

    login true/false
    colocando botões para quando o usuário fazer login tirar opções desnecessárias

--- Frontend/src/components/tags/util.jsx
+++ Frontend/src/components/tags/util.jsx
@@ -1,7 +1,20 @@
 import './index.scss'
 import { Link } from 'react-router'
+import { useEffect, useState } from "react"
 
 export default function Tags() {
+    const [logado, setLogado] = useState(false)
+
+    useEffect(() => {
+        const nomeUsuario = localStorage.getItem("USUARIO");
+        
+        if (nomeUsuario != undefined || nomeUsuario != null) {
+            setLogado(true)
+        } else {
+            setLogado(false)
+        }
+    }, [])
+
     return (
         <section className='section-tags' >
             <div className='container-tags'>
@@ -18,11 +31,26 @@ export default function Tags() {
                     <Link to='/contato'> Contato </Link>
                     <Link to='/Marcar agendamento'> Marcar agendamento </Link>
                     <Link to="/Comoajudar">Como apoiar</Link>
-
                 </div>
+
+
                 <div className='botao-login'>
-                    <Link to='/login' className='login'> Login </Link>
-                    <Link to='/cadastro'> <button> Cadastra-se </button> </Link>
+                    
+                    
+                    {logado ? (
+                        // Usuário logado - mostra botões adicionais
+                        <div>
+                            <Link to='/AgendamentoUser'>Informações usuário</Link>
+                            <Link to='/listar'>Listar</Link>
+                            <span>Logado</span>
+                        </div>
+                    ) : (
+
+                <div>
+                            <Link to='/login' className='login'> Login </Link>
+                            <Link to='/cadastro'> <button> Cadastra-se </button> </Link>
+                        </div>
+                    )}
                 </div>
             </div>
         </section>
