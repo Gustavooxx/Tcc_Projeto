5d95ec5777bde30293ed20d907b5357d092dc89f
Tukis-bit <arthur01raphael@gmail.com>
2025-11-04 14:02:29 -0300

    email de voluntariado

--- backend/src/repository/voluntariosRepository.js
+++ backend/src/repository/voluntariosRepository.js
@@ -1,4 +1,5 @@
 import connection from "./connetion.js";
+import transporter from "./email.js";
 
 
 export async function cadastroVoluntario(infos,usuario_id){
@@ -10,7 +11,25 @@ values
 
 const [info] = await connection.query(comando,[infos.nome,infos.email,infos.cpf,infos.telefone,infos.disponibilidade,infos.mensagem,usuario_id]);
 
+const assunto = `Agradecemos por se tornar voluntário(a)! `;
+const texto = `Olá, ${infos.nome}!
+
+Agradecemos por seu interesse em se tornar parte da nossa equipe de voluntários(as).
+Sua vontade de ajudar faz toda a diferença e nos inspira a continuar essa missão tão importante. 🌟
+
+Em breve, você receberá um e-mail de confirmação informando se sua inscrição foi aprovada ou não.
+De qualquer forma, agradecemos profundamente por dedicar seu tempo e disposição para apoiar essa causa. ❤️
+
+Caso seja aceito(a), enviaremos também todas as orientações sobre os próximos passos e atividades disponíveis.
+
+Com gratidão,
+Equipe Doe Vida`
+
+await transporter.sendMail({
+      to: infos.email,
+    subject: assunto,
+    text: texto
+});
+
 return info.insertId;
-
-
 }
